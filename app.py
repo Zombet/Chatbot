85d6df9a87da54da005ce5d25139d3e354522e77
Ifrah <sadiq21ifrah@gmail.com>
2025-11-14 23:45:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAAQaJyXrila2YjMnHPcX0nxUssDP5YBOI")
+genai.configure(api_key="Your_API_key")
 model = genai.GenerativeModel("models/gemini-2.5-flash")
 
 app = Flask(__name__)
